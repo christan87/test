438e3843df35cb513d7f8c1fc97c503f069c80fc
Christan Prices <christanprices@75-144-18-237-BusName-sfpine.ca.hfc.comcastbusiness.net>
2023-01-20 05:23:40 -0800

    package.json

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 var express = require('express')
+const severless = require('serverless-http');
 var app = express()
 
 const defaultRoute = require('../routes/default')
@@ -6,6 +7,8 @@ const defaultRoute = require('../routes/default')
 app.use(express.json());
 app.use('/test', defaultRoute)
 
-app.listen(3000, function () {
-  console.log(`CORS-enabled web server listening on port 80`)
-})
+module.exports.handler = severless(app)
+
+// app.listen(3000, function () {
+//   console.log(`CORS-enabled web server listening on port 80`)
+// })
