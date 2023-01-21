098caf7adf4582e7203fdf7edf23833ade15fde5
Christan Prices <christanprices@75-144-18-237-BusName-sfpine.ca.hfc.comcastbusiness.net>
2023-01-21 08:37:38 -0800

    toml and server changes

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
 var express = require('express')
+import {json} from 'express';
 const severless = require('serverless-http');
-var app = express()
+// var app = express()
 
 const defaultRoute = require('../routes/default')
 
-app.use(express.json());
-app.use('/.netlify/functions/test', defaultRoute)
+export async function handler(event, context){
+  var app = express()
+  app.use(json());
+  app.use('/test', defaultRoute)
+  return severless(app)(event, context)
+}
 
-module.exports.handler = severless(app)
+// app.use(express.json());
+// app.use('/.netlify/functions/test', defaultRoute)
+
+// module.exports.handler = severless(app)
 
 // app.listen(3000, function () {
 //   console.log(`CORS-enabled web server listening on port 80`)
